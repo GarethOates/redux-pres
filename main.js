c768339b51445e417d18b520ef01558384e9abc4
Gareth Oates <gareth.oates@itslearning.com>
2017-03-27 16:34:48 +0200

    Simple redux demo

--- main.js
+++ main.js
@@ -1,21 +1,40 @@
-var State = {}; // <-- this is state. simple.
+const Store = Redux.createStore(reducer);
+
+function reducer(state, action) {
+    if (!state) {
+        state = {
+            'things': []
+        };
+
+        return state;
+    }
+
+    if (action.payload.things) {
+        state.things = action.payload.things;
+    }
+
+    return state;
+}
+
+Store.subscribe(onStateUpdate);
 
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////// helpers ///////////////////////////////////////
 
+// this code updates your components in your application
 function onStateUpdate() {
-    const container = document.getElementById('things');
+    const state = Store.getState();
 
-    if (State.things) {
-        State.things.forEach(addListItem);
+    if (state.things) {
+        state.things.forEach(addListItem);
     }
 }
 
-
 function addListItem(text) {
+    const container = document.getElementById('things');
     const item = document.createElement('li');
 
     item.textContent = text;
 
-    document.body.appendChild(item);
+    container.appendChild(item);
 }
